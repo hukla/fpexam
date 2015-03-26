22080cdb8abf306b5c2e35fb39b9b458593a8d35
hukla <hukla@snu.ac.kr>
2015-03-26 15:31:56 +0900

    changed models a bit
    Former-commit-id: 84826aaf174fdfce693d4b3af2d07e737747d084

--- src/model/FPTree.java
+++ src/model/FPTree.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Vector;
 
 public class FPTree {
-	private List<FPHeaderTableColumn> headerTable = new ArrayList<FPHeaderTableColumn>();
+	private Vector<FPNode> headerTable = new Vector<FPNode>();
 	private FPNode root;
 	private int minsup;
+	private Map<String, Integer> freqPatterns;
 	
 	public FPTree() {
 		root = null;
@@ -16,7 +18,6 @@ public class FPTree {
 
 	public FPTree(String[] DB, Map<Character, Integer> flist, int minsup) {
 		root = new FPNode();
-		root.setNode(null);
 		this.minsup = minsup;
 
 		FPNode tempNode;
@@ -30,33 +31,36 @@ public class FPTree {
 				item = t.charAt(i);
 				int cursup = 1;
 				int j = 0;
-				for (; !(tempNode.getChildren().isEmpty()) && j < tempNode.getChildren().size(); j++) {
-					if (tempNode.getChildren().get(j).getNode().getItem() == item) {
-						cursup = tempNode.getChildren().get(j).getNode().getSupport() + 1;
-						break;
+				
+				if(!tempNode.getChildren().isEmpty()) {
+					for (; j < tempNode.getChildren().size(); j++) {
+						if (tempNode.getChildren().get(j).getItem() == item) {
+							cursup = tempNode.getChildren().get(j).getSupport() + 1;
+							break;
+						}
 					}
 				}
 
-				newNode = new FPNode(new FPNodeContainer(cursup, item, tempNode.getNode()));
+				newNode = new FPNode(cursup, item, tempNode);
 
 				if (cursup == 1) {
 					tempNode.putChild(newNode);
 
 					int tableIdx = tableHasItem(item);
 					if (tableIdx == -1) {
-						headerTable.add(new FPHeaderTableColumn(item));
-						headerTable.get(headerTable.size()-1).setNodeLink(newNode.getNode());
+						headerTable.add(newNode);
+						headerTable.get(headerTable.size()-1).setNodeLink(newNode);
 					} else {
-						FPNodeContainer tempNodeLink = headerTable.get(tableIdx).getNodeLink();
+						FPNode tempNodeLink = headerTable.get(tableIdx).getNodeLink();
 						while (tempNodeLink.getNodeLink() != null) {
 							tempNodeLink = tempNodeLink.getNodeLink();
 						}
-						tempNodeLink.setNodeLink(newNode.getNode());
+						tempNodeLink.setNodeLink(newNode);
 					}
 					
 					tempNode = newNode;
 				} else {
-					tempNode.getChildren().get(j).getNode().setSupport(cursup);
+					tempNode.getChildren().get(j).setSupport(cursup);
 					
 					tempNode = tempNode.getChildren().get(j);
 				}
@@ -67,7 +71,7 @@ public class FPTree {
 //		System.out.println(headerTable);
 	}
 
-	public FPTree(List<FPHeaderTableColumn> headerTable, FPNode root, int minsup) {
+	public FPTree(Vector<FPNode> headerTable, FPNode root, int minsup) {
 		this.headerTable = headerTable;
 		this.root = root;
 		this.minsup = minsup;
@@ -75,51 +79,52 @@ public class FPTree {
 
 	int tableHasItem(char item) {
 		for (int i = 0; i < headerTable.size(); i++) {
-			if (headerTable.get(i).getItemName() == item) {
+			if (headerTable.get(i).getItem() == item) {
 				return i;
 			}
 		}
 		return -1;
 	}
 	
-	FPNode hasSinglePrefixPath() {
-		FPNode tail = root;
-		//TODO
-		return tail;
-	}
-
 	public void growth() {
-		FPNode z;
-		if((z = hasSinglePrefixPath()) != null) {
-			// if tree contains a single prefix path Z
-			List<FPHeaderTableColumn> tempHtable = headerTable;
-			int i = 0;
-			for(; i < headerTable.size(); i++) {
-				if(headerTable.get(i).getItemName() == z.getNode().getItem()) {
-					break;
+		growth(root, null, headerTable);
+	}
+	
+	public void growth(FPNode root, String base, Vector<FPNode> headerTable) {
+		for(FPNode iteminTree : headerTable) {
+			String currentPattern = (base != null ? base : "") + (base != null ? " " : "") + iteminTree.getItem();
+			int supcur = 0; // support of current pattern
+			Map<String, Integer> conditionalPatternBase = new HashMap<String, Integer>();
+			while (iteminTree.getNodeLink() != null) {
+				iteminTree = iteminTree.getNodeLink();
+				supcur += iteminTree.getSupport();
+				String conditionalPattern = null;
+				FPNode conditionalItem = iteminTree.getParent();
+				
+				while(conditionalItem != null) {
+					conditionalPattern = conditionalItem.getItem()+ " " + (conditionalPattern != null ? conditionalPattern : "");
+					conditionalItem = conditionalItem.getParent();
 				}
+				 //TODO
 			}
-			tempHtable.remove(i);
-
-			FPTree subtree = new FPTree(tempHtable, z, minsup);
-			subtree.growth();
-			generateARs();
-		} else {
-			//TODO generate pattern????
 		}
 	}
 	
-	public void generateARs() {
-		// TODO
-	}
-	
-	
-	public List<FPHeaderTableColumn> getHeaderTable() {
+
+	public Vector<FPNode> getHeaderTable() {
 		return headerTable;
 	}
 
-	public void setHeaderTable(List<FPHeaderTableColumn> headerTable) {
+	public void setHeaderTable(Vector<FPNode> headerTable) {
 		this.headerTable = headerTable;
+	}
+
+	public int getMinsup() {
+		return minsup;
+	}
+
+	public void setMinsup(int minsup) {
+		this.minsup = minsup;
 	}
 
 	public FPNode getRoot() {
